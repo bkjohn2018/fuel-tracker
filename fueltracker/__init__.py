041b018a83339f06737c4f10d12520854ffdec46
bkjohn2018 <briankenjohn@msn.com>
2025-08-26 14:48:15 -0600

    fix: restore corrupted __init__.py file after whitespace cleanup

--- fueltracker/__init__.py
+++ fueltracker/__init__.py
@@ -1,1 +1,8 @@
-"""Fuel Tracker - A Python package for tracking fuel prices and consumption."""__version__ = "0.1.0"__author__ = "Brian"__description__ = "A Python package for tracking fuel prices and consumption"# Version info for easy accessVERSION = __version__
+"""Fuel Tracker - A Python package for tracking fuel prices and consumption."""
+
+__version__ = "0.1.0"
+__author__ = "Brian"
+__description__ = "A Python package for tracking fuel prices and consumption"
+
+# Version info for easy access
+VERSION = __version__
